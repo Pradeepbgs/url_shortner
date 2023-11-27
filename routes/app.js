646a43f536b6b62da272bcd322df91bb03684c52
pradeep <pradeep>
2023-11-27 19:59:16 +0530

    added '/' res.send

--- routes/app.js
+++ routes/app.js
@@ -6,6 +6,10 @@ const router = express.Router()
 
 router.post('/', handleShortId)
 
+router.get('/', function(req, res) {
+    res.send("hello")
+})
+
 router.get('/:shortId', async function(req, res) {
     const shortId = req.params.shortId;
     const entry = await Url.findOne(
